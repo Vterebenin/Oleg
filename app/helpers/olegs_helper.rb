15a4d1a1cd1cf833b51072f58bfcce9561024f60
Vterebenin <niceserver11@mail.ru>
2018-05-13 23:13:00 +0300

    Proof of a concept

--- app/helpers/olegs_helper.rb
+++ app/helpers/olegs_helper.rb
@@ -1,25 +1,22 @@
 module OlegsHelper
 	def film_to_oleg(film)
 		film = film.split(' ')
-	#	if film.length > 1
-	#		n = ""
-	#		hash = Hash[film.map.with_index.to_a]
-	#		i = 0
-	#		until (i >= film.count) || film.include?("Oleg") do
-	#			n = film.sample
-	#			s = hash[n]
-	#			n = "Oleg" if n.length > 3
-	#			film[s] = n
-	#			i += 1
-	#		end
-	#		if film.include?("Oleg")
-	#			film.join(" ")
-	#		else
-	#			film.join(" ")
-	#		end
-	#	else
-	#		film_to_oleg(film)
-	#	end
+		p film
+		n = ""
+		hash = Hash[film.map.with_index.to_a]
+		i = 0
+		until (i >= film.count) || film.include?("Oleg") do
+			n = film.sample
+			s = hash[n]
+			n = "Oleg" if n.length > 3
+			film[s] = n
+			i += 1
+		end
+		if film.include?("Oleg")
+			film.join(" ")
+		else
+			film.join(" ")
+		end
 	end
 	def get_random_film_name
 		r = Random.new			
